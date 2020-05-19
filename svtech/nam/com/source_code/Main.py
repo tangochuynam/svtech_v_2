b34ce92397e3854f2570ab8889a798aadb402e7f
hunglnm <hunglnm2307@gmail.com>
2020-05-19 13:50:16 +0700

    No changes

--- svtech/nam/com/source_code/Main.py
+++ svtech/nam/com/source_code/Main.py
@@ -53,7 +53,7 @@ class Main:
         check_continute = 'y'
         #'THA02SSN','LSN99LLI'
         #'LSN00LLI','THA00THA'
-        for hostname in ['HDG03NGG']:
+        for hostname in ['HDG01TNC']:
             print ("hostname: " + hostname)
             router = Router()
             router.hostname = hostname
